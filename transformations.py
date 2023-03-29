748d0af89153c2717f1dd003424d6d6e8159ff89
Bekhzod Olimov <50166164+bekhzod-olimov@users.noreply.github.com>
2023-03-29 16:32:19 +0900

    transformations script

--- transformations.py
+++ transformations.py
@@ -1,6 +1,18 @@
+# Import library
 from torchvision import transforms as T
 
 def get_transformations():
+    
+    """
+    
+    This function intiializes train and validation set transformations.
+    
+    
+    Output:
+    
+        transformations   -  a list of transformations for train and validation, respectively, list.
+        
+    """
 
     return [T.Compose([T.Resize((704, 1056)),
                       T.RandomVerticalFlip(),
