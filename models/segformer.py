c164ff58c9697de78864262df19fc747b8d129fc
Bekhzod Olimov <50166164+bekhzod-olimov@users.noreply.github.com>
2023-04-06 17:29:31 +0900

    Update segformer.py

--- models/segformer.py
+++ models/segformer.py
@@ -369,18 +369,18 @@ class SegFormerSegmentationHead(nn.Module):
     
     def __init__(self, channels: int, num_classes: int, num_features: int = 4):
         super().__init__()
-        self.fuse = nn.Sequential(
+        self.sequence = nn.Sequential(
             nn.Conv2d(channels * num_features, channels, kernel_size = 1, bias = False),
-            nn.ReLU(), # why relu? Who knows
-            nn.BatchNorm2d(channels) # why batchnorm and not layer norm? Idk
+            nn.ReLU(), 
+            nn.BatchNorm2d(channels) 
         )
-        self.predict = nn.Conv2d(channels, num_classes, kernel_size = 1)
+        self.out_layer = nn.Conv2d(channels, num_classes, kernel_size = 1)
 
     def forward(self, features):
-        x = torch.cat(features, dim=1)
-        x = self.fuse(x)
-        x = self.predict(x)
-        return x
+        
+        x = torch.cat(features, dim = 1)
+        x = self.sequence(x)
+        return self.out_layer(x)
     
 class SegFormerDecoderBlock(nn.Sequential):
     def __init__(self, in_channels: int, out_channels: int, scale_factor: int = 2):
